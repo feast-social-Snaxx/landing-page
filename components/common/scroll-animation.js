1e7f4018cae6b4240cafd7e5addd05fef97b9781
0x0-github <6e6f6f6e65@protonmail.com>
2023-02-21 07:38:10 +0100

    Added some animations

---
+++ components/common/scroll-animation.js
@@ -0,0 +1,84 @@
+import { useEffect, useRef, useState } from "react";
+
+function useElementOnScreen(ref, rootMargin = "0px") {
+  const [isIntersecting, setIsIntersecting] = useState(true);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setIsIntersecting(entry.isIntersecting);
+      },
+      { rootMargin }
+    );
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+    return () => {
+      if (ref.current) {
+        observer.unobserve(ref.current);
+      }
+    };
+  }, []);
+  return isIntersecting;
+}
+
+const AnimateIn = ({ from, to, children, className, style = {}, animStyle = { transition: "600ms ease-in-out" } }) => {
+  const ref = useRef(null);
+  const onScreen = useElementOnScreen(ref);
+  return (
+    <div
+      ref={ref}
+      className={className}
+      style={
+        onScreen
+          ? {
+            ...animStyle,
+            ...to,
+            ...style
+          }
+          : {
+            ...animStyle,
+            ...from,
+            ...style
+          }
+      }
+    >
+      {children}
+    </div>
+  );
+};
+
+const FadeIn = ({ children, className, animStyle= { transition: ".75 ease-in-out" }, style = {} }) => (
+  <AnimateIn from={{ opacity: 0 }} to={{ opacity: 1 }} className={className} style={style} animStyle={animStyle}>
+    {children}
+  </AnimateIn>
+);
+
+const FadeUp = ({ children, className }) => (
+  <AnimateIn
+    from={{ opacity: 0, translate: "0 2rem" }}
+    to={{ opacity: 1, translate: "none" }}
+    className={className}
+  >
+    {children}
+  </AnimateIn>
+);
+
+const ScaleIn = ({ children, className }) => (
+  <AnimateIn
+    from={{ scale: "0" }}
+    to={{ scale: "1" }}
+    className={className}
+    animStyle={{ transition: ".75s ease-in-out" }}
+  >
+    {children}
+  </AnimateIn>
+);
+
+const ScrollAnimation = {
+  FadeIn,
+  FadeUp,
+  ScaleIn
+}
+
+export default ScrollAnimation;
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import MainContainer from '../components/common/main-container';
 // import { useRouter } from 'next/router';
 import { useRef } from 'react';
 import Image from 'next/image';
+import ScrollAnimation from '../components/common/scroll-animation';
 
 const TOOLS_ITEMS = [
   {
@@ -58,22 +59,26 @@ export default function Home() {
 
       <section className='f-col c-primary index-row-2 b-marg-l'>
         <div className='f-row jc-sa ai-c f-wrap'>
-          <Image
-            className='sticker1'
-            alt="Presentation first sticker image"
-            src="/sticker1.webp"
-            width={!isMobile ? "655" : "450"}
-            height={!isMobile ? "640" : "440"}
-          />
-
+          <ScrollAnimation.ScaleIn>
+            <Image
+              className='sticker1'
+              alt="Presentation first sticker image"
+              src="/sticker1.webp"
+              width={!isMobile ? "655" : "450"}
+              height={!isMobile ? "640" : "440"}
+            />
+          </ScrollAnimation.ScaleIn>
           <div className={`padd-s-m ${!isMobile ? "lhw" : "lfw"}`}>
-            <div className='spacer-m'></div>
-            <h2 className='fs-xl fw-l'>What is SNAXX ?</h2>
-            <p className='fs-xm'>Snaxx is the ultimate destination for food enthusiasts looking to display their creativity and get paid for it. With its user-friendly tools and supportive community, Snaxx provides an outlet to showcase your culinary creations, food journey, daily food routines, food critic, diets, family recipes, and so much more. Whether you're a seasoned chef or just enjoy cooking for fun, Snaxx is the perfect place for you to share your passion and get rewarded for it.</p>
+            <ScrollAnimation.FadeUp>
+              <div className='spacer-m'></div>
+              <h2 className='fs-xl fw-l'>What is SNAXX ?</h2>
+              <p className='fs-xm'>Snaxx is the ultimate destination for food enthusiasts looking to display their creativity and get paid for it. With its user-friendly tools and supportive community, Snaxx provides an outlet to showcase your culinary creations, food journey, daily food routines, food critic, diets, family recipes, and so much more. Whether you're a seasoned chef or just enjoy cooking for fun, Snaxx is the perfect place for you to share your passion and get rewarded for it.</p>
+            </ScrollAnimation.FadeUp>
           </div>
         </div>
 
         <div className='f-row jc-c ai-c f-wrap'>
+        <ScrollAnimation.ScaleIn>
           <Image
             className='arrow-img'
             alt="Accent colored arrow image"
@@ -81,26 +86,33 @@ export default function Home() {
             width={!isMobile ? "347" : "280"}
             height={!isMobile ? "347" : "280"}
           />
+          </ScrollAnimation.ScaleIn>
         </div>
 
         <div className='f-row jc-sa ai-c f-wrap'>
           <div className={`f-col ${!isMobile ? "lhw" : "lfw"}`}>
             <div className="padd-s-m">
-              <h2 className='fs-xl fw-l'>Empowering creators</h2>
-              <p className='fs-xm'>Our tools and features make it simple to grow your audience and connect with people from around the world through shared love of food. Whether you're an athlete, lifestyle creator, artist, fitness guru or anything in between, everyone can showcase their unique cooking style on Snaxx.</p>
+              <ScrollAnimation.FadeUp>
+                <h2 className='fs-xl fw-l'>Empowering creators</h2>
+                <p className='fs-xm'>Our tools and features make it simple to grow your audience and connect with people from around the world through shared love of food. Whether you're an athlete, lifestyle creator, artist, fitness guru or anything in between, everyone can showcase their unique cooking style on Snaxx.</p>
+              </ScrollAnimation.FadeUp>
             </div>
             <div className='padd-s-m'>
-              <h2 className='fs-xl fw-l'>Directly monetize</h2>
-              <p className='fs-xm'>Strengthen your bond with your audience by offering unique subscriber-only content. Effortlessly monetize your creativity with our paywall system, and turn it into a growing source of recurring income.</p>
+              <ScrollAnimation.FadeUp>
+                <h2 className='fs-xl fw-l'>Directly monetize</h2>
+                <p className='fs-xm'>Strengthen your bond with your audience by offering unique subscriber-only content. Effortlessly monetize your creativity with our paywall system, and turn it into a growing source of recurring income.</p>
+              </ScrollAnimation.FadeUp>
             </div>
           </div>
-          <Image
-            className='sticker2'
-            alt="Presentation second sticker image"
-            src="/sticker2.webp"
-            width={!isMobile ? "720" : "500"}
-            height={!isMobile ? "767" : "533"}
-          />
+          <ScrollAnimation.ScaleIn>
+            <Image
+              className='sticker2'
+              alt="Presentation second sticker image"
+              src="/sticker2.webp"
+              width={!isMobile ? "720" : "500"}
+              height={!isMobile ? "767" : "533"}
+            />
+          </ScrollAnimation.ScaleIn>
         </div>
       </section>
 
@@ -134,10 +146,10 @@ export default function Home() {
         <div className='f-row jc-c f-wrap tool-container b-marg-l'>
           {
             TOOLS_ITEMS.map((itm, i) => {
-              return <div key={`i-t-i-${i}`} className='c-primary tool-item f-col jc-c padd-s-m' style={{ backgroundImage: `url(/${itm.background})` }}>
+              return <ScrollAnimation.FadeIn key={`i-t-i-${i}`} className='c-primary tool-item f-col jc-c padd-s-m' style={{ backgroundImage: `url(/${itm.background})` }} animStyle={ { transition: `.75s ease-in-out ${0.25 * (i + 1)}s`}}>
                 <h3 className='fs-xl fw-l tool-marg'>{itm.title}</h3>
                 <p className='fs-xm tool-marg'>{itm.description}</p>
-              </div>;
+              </ScrollAnimation.FadeIn>;
             })
           }
         </div>
